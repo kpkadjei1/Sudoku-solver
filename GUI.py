52910b12b2515b00443e99851d0581d10d4c3561
Kelvin Adjei <kadjei1@swarthmore.edu>
2020-03-20 00:50:52 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -52,7 +52,7 @@ class Grid:
     def draw(self):
         # Draw Grid Lines
         gap = self.width / 9
-        for i in range(self.rows+1):
+        for i in range(self.rows + 1):
             if i%3 == 0 and i != 0:
                 thick = 4
             else:
@@ -198,58 +198,58 @@ class Cube:
         self.temp = val
 
 
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
+    def find_empty(bo):
+        for i in range(len(bo)):
+            for j in range(len(bo[0])):
+                if bo[i][j] == 0:
+                    return (i, j)  # row, col
 
-    return None
+        return None
 
 
-def valid(bo, num, pos):
-    # Check row
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
-
-    # Check column
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
-
-    # Check box
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+    def valid(bo, num, pos):
+        # Check row
+        for i in range(len(bo[0])):
+            if bo[pos[0]][i] == num and pos[1] != i:
                 return False
 
-    return True
+        # Check column
+        for i in range(len(bo)):
+            if bo[i][pos[1]] == num and pos[0] != i:
+                return False
+
+        # Check box
+        box_x = pos[1] // 3
+        box_y = pos[0] // 3
+
+        for i in range(box_y*3, box_y*3 + 3):
+            for j in range(box_x * 3, box_x*3 + 3):
+                if bo[i][j] == num and (i,j) != pos:
+                    return False
+
+        return True
 
 
-def redraw_window(win, board, time, strikes):
-    win.fill((255,255,255))
-    # Draw time
-    fnt = pygame.font.SysFont("comicsans", 40)
-    text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
-    win.blit(text, (540 - 160, 560))
-    # Draw Strikes
-    text = fnt.render("X " * strikes, 1, (255, 0, 0))
-    win.blit(text, (20, 560))
-    # Draw grid and board
-    board.draw()
+    def redraw_window(win, board, time, strikes):
+        win.fill((255,255,255))
+        # Draw time
+        fnt = pygame.font.SysFont("comicsans", 40)
+        text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
+        win.blit(text, (540 - 160, 560))
+        # Draw Strikes
+        text = fnt.render("X " * strikes, 1, (255, 0, 0))
+        win.blit(text, (20, 560))
+        # Draw grid and board
+        board.draw()
 
 
-def format_time(secs):
-    sec = secs%60
-    minute = secs//60
-    hour = minute//60
+    def format_time(secs):
+        sec = secs%60
+        minute = secs//60
+        hour = minute//60
 
-    mat = " " + str(minute) + ":" + str(sec)
-    return mat
+        mat = " " + str(minute) + ":" + str(sec)
+        return mat
 
 
 def main():
